5e32f7483315032bc3b12c176dc704da3d13e9c3
Achille Pellissier <achillepellissier@gmail.com>
2020-12-09 11:03:42 +0100

    first real front

--- app.py
+++ app.py
@@ -2,4 +2,14 @@ import streamlit as st
 
 import requests
 
-st.markdown('Bonjour je suis un test')
+api_url = 'https://projectspotify-76glfmoaxq-ew.a.run.app'
+
+st.markdown('#Please input the song uri')
+
+uri = st.text_input('Spotify URI', 'URI here please')
+response = requests.get(f'{api_url}/predict_genre/{uri}')
+prediction = response.json()
+pred = prediction['prediction']
+
+st.markdown(f'''###The genre is:
+#{pred}''')
