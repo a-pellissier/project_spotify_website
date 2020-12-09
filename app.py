bd04d164dd1c73ef1792697684036874a912cba9
Achille Pellissier <achillepellissier@gmail.com>
2020-12-09 11:15:17 +0100

    template for further modification

--- app.py
+++ app.py
@@ -4,12 +4,22 @@ import requests
 
 api_url = 'https://projectspotify-76glfmoaxq-ew.a.run.app'
 
-st.markdown('#Please input the song uri')
+st.markdown('''
+    # Please input the song uri''')
 
-uri = st.text_input('Spotify URI', 'URI here please')
-response = requests.get(f'{api_url}/predict_genre/{uri}')
-prediction = response.json()
-pred = prediction['prediction']
+uri = st.text_input('Spotify URI')
 
-st.markdown(f'''###The genre is:
-#{pred}''')
+if st.button('Make the magic happen'):
+    # print is visible in server output, not in the page
+    st.write('Querying the amazing API!')
+    response = requests.get(f'{api_url}/predict_genre/{uri}')
+    if response.status_code == 200:
+        prediction = response.json()
+        pred = prediction['prediction']
+        st.markdown(f'''
+        ### The genre is:
+        # {pred}''')
+    else:
+        st.markdown('''## Spotify caused an error 😞''')
+else:
+    st.write('')
